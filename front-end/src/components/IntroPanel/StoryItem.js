e0a0e3c9b1c4ca551d1ed6d0f6cace13a2fa24f0
Phuoc Do <binanhphuoc@gmail.com>
2018-08-03 09:48:11 -0400

    Fix about page

---
+++ front-end/src/components/IntroPanel/StoryItem.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './intropanel.css';
+
+
+class StoryItem extends Component {
+ 
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      bodyClass:'hidden'
+    }
+
+  }
+
+  render(){
+
+    return(
+        <div className="intropanel div">
+        <img style={{height:100+"vh", width:100+"vw"}} src={this.props.story.fileData} />
+        {/*<p className="legend">{story.title}</p>*/}
+        {this.props.story.done && 
+        <div 
+          style={{top:"100px", left:"100px"}} 
+          className="intropanel title"
+          onMouseEnter={()=>{
+            this.setState({bodyClass:'show'});
+          }}
+          onMouseLeave={()=>{
+            this.setState({bodyClass:'hidden'});
+          }}
+        >
+          {this.props.story.title}
+        </div>}
+        
+        <div 
+          style={{top:"200px", left:"100px"}} 
+          className={"intropanel subtitle "+this.state.bodyClass}
+        > {/*"intropanel subtitle hidden"*/}
+        {this.props.story.subtitle}
+        </div>
+        
+
+      </div>
+    )};
+}
+ 
+
+/*
+importAll(r) {
+  let images = [];
+  r.keys().map((item, index) => { images.push({original:r(item)}); });
+  console.log('size: ' + images.length);
+  return images;
+}
+
+images = this.importAll(require.context('./Intropanel/images', false, /\.(png|jpe?g|svg)$/));*/
+
+export default StoryItem;
--- front-end/src/components/IntroPanel/intropanel.js
+++ front-end/src/components/IntroPanel/intropanel.js
@@ -5,85 +5,60 @@ import 'react-responsive-carousel/lib/styles/carousel.css';
 import { Carousel } from 'react-responsive-carousel';
 import iconPrev from './slider-left-arrow.svg';
 import iconNext from './slider-right-arrow.svg';
+import StoryAPI from '../AdminManager/storyAPI';
+import StoryItem from './StoryItem';
 
 
 class Intropanel extends Component {
  
-  importAll(r) {
-    let images = [];
-    r.keys().map((item, index) => { images.push({original:r(item)}); });
-    console.log('size: ' + images.length);
-    return images;
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      stories: [],
+    }
+
+    StoryAPI.getAllStory((stories)=>{
+      this.setState({stories});
+      
+      for (var i = 0; i < stories.length; i++)
+      {
+          var stories = this.state.stories.slice();
+          stories[i].fileData=require('../AdminManager/empty.png');
+          stories[i].done = false;
+          stories[i].index = i;
+          this.setState({stories});
+      }
+    },
+    (file, index) => {
+        var stories = this.state.stories.slice();
+        stories[index].fileData=file;
+        stories[index].done = true;
+        this.setState({stories});
+    })
+
+    this.mapping = this.mapping.bind(this);
   }
   
+  icon = {
+    prev: iconPrev,
+    next: iconNext
+  }
   //images = this.importAll(require.context('./Intropanel/images', false, /\.(png|jpe?g|svg)$/));
+
+  mapping(story)
+  {
+    return(
+      <StoryItem story={story}/>
+    )
+  }
 
   render(){
 
-    var icon = {
-      prev: iconPrev,
-      next: iconNext
-    }
-
     return(
-  <Carousel showStatus={false} showThumbs={false} icon={icon}>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg" />
-      <p className="legend">Legend 1</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-2.jpg" />
-      <p className="legend">Legend 2</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-3.jpg" />
-      <p className="legend">Legend 3</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-4.jpg" />
-      <p className="legend">Legend 4</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-5.jpg" />
-      <p className="legend">Legend 5</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-6.jpg" />
-      <p className="legend">Legend 6</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-7.jpg" />
-      <p className="legend">Legend 7</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-8.jpg" />
-      <p className="legend">Legend 8</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-9.jpg" />
-      <p className="legend">Legend 9</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-10.jpg" />
-      <p className="legend">Legend 10</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-11.jpg" />
-      <p className="legend">Legend 11</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-12.jpg" />
-      <p className="legend">Legend 12</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-13.jpg" />
-      <p className="legend">Legend 13</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-14.jpg" />
-      <p className="legend">Legend 14</p>
-    </div>
-  </Carousel>
+      <Carousel showStatus={false} showThumbs={false} icon={this.icon}>
+        {this.state.stories.map(this.mapping)}
+      </Carousel>
     )};
 }
  
