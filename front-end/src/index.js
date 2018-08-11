8de60820fc499cb1852355b892c651cf3bc38b58
Phuoc Do <binanhphuoc@gmail.com>
2018-08-11 03:29:44 -0500

    fix bug

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import registerServiceWorker from './registerServiceWorker';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
